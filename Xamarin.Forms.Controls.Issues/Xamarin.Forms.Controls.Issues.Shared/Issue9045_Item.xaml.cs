ced0c91781e1dbfe6b86cb8912f0dd915eec4f41
Matthew Richardson <mrichardson@bethel.jw.org>
2020-09-02 10:58:49 +0100

    Repro for #9045

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue9045_Item.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin.Forms.Controls.Issues
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Issue9045_Item : Grid
+    {
+        public Issue9045_Item()
+        {
+            InitializeComponent();
+        }
+
+		protected override void OnBindingContextChanged()
+		{
+			base.OnBindingContextChanged();
+
+			if (BindingContext is OfflineMonkey monkey)
+			{
+				Debug.WriteLine($"Bound {nameof(Issue9045_Item)}_{Id} for {monkey.Name}");
+			}
+		}
+	}
+}
